93f502573193b94be45d6431b7493b3951cc4418
rdlynx19 <pushkardave.vnit@gmail.com>
2024-09-29 19:15:36 -0500

    Created the load service, setup movement for turtle using publisher. Added a launch file to start turtle_control and turtlesim together

--- turtle_control/setup.py
+++ turtle_control/setup.py
@@ -9,18 +9,18 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml', 'launch/start_turtle_control.launch.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='redhairedlynx',
     maintainer_email='pushkardave.vnit@gmail.com',
-    description='TODO: Package description',
+    description='Package to control the turtle',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'way_node = turtle_control.waypoint_node:main'
+            'waypoint = turtle_control.waypoint_node:main'
         ],
     },
 )
--- turtle_control/turtle_control/waypoint_node.py
+++ turtle_control/turtle_control/waypoint_node.py
@@ -4,12 +4,38 @@ from rclpy.node import Node
 import rclpy.parameter
 from std_srvs.srv import Empty
 
+from geometry_msgs.msg import Twist, Vector3
+
+from turtle_interfaces.srv import Waypoints
+
+from turtlesim.srv import Spawn, Kill
+# Work ahead from this point, think about using turtlesim nodes without listing in dependency
+
+
+def turtle_twist(linear_vel, angular_vel):
+    """Create a twist velocity suitable for a turtle
+
+        Args:
+            linear_vel (list of floats): the linear velocities
+            angular_vel (list of floats): the angular velocities
+
+        Returns:
+            Twist - a 2D twist object corresponding to linear/angular velocity
+    """
+    return Twist(linear = Vector3(x = linear_vel[0], y = linear_vel[1], z = linear_vel[2]),
+                    angular = Vector3(x = angular_vel[0], y = angular_vel[1], z = angular_vel[2]))
+
+
+
+
 class Waypoint(Node):
     """The waypoint Node"""
 
     def __init__(self):
         super().__init__('waypoint')
-        self.get_logger().info('way_node')
+        self.get_logger().info('waypoint_node')
+
+        self.pub = self.create_publisher(Twist, "cmd_vel", 10)
 
         self.declare_parameter('frequency',90)
         # Can change the parameter value during runtime but change is not effective
@@ -20,11 +46,15 @@ class Waypoint(Node):
         self.timer = self.create_timer((1/self.freq),self.timer_callback)
         self._srv = self.create_service(Empty,'toggle', self.toggle_callback)
 
+        self._interface_srv = self.create_service(Waypoints, 'load', self.waypoints_callback)
+
     def timer_callback(self):
         tim_turt_state = self.get_parameter('turtle_state').get_parameter_value().string_value
         if(tim_turt_state == 'MOVING'):
 
             self.get_logger().debug("Issuing Command!") 
+            twist = turtle_twist([4.5, 0.0, 0.0], [0.0, 0.0, -2.0])
+            self.pub.publish(twist)
     
     def toggle_callback(self,request,response):
         turt_state = self.get_parameter('turtle_state').get_parameter_value().string_value
@@ -44,7 +74,8 @@ class Waypoint(Node):
             self.set_parameters(stp_switch)
             return response
             
-   
+    def waypoints_callback(self, request, response):
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
