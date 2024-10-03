06f8dd343d38719f07d3c3af74e5203efd200b0e
rdlynx19 <pushkardave.vnit@gmail.com>
2024-10-03 01:02:18 -0500

    Drew X, placed turtle at first waypoint and computed the stright line distance from traversing the waypoints

--- turtle_control/turtle_control/waypoint_node.py
+++ turtle_control/turtle_control/waypoint_node.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 
+import math
+
 import rclpy.parameter
 from std_srvs.srv import Empty
 
@@ -8,7 +10,9 @@ from geometry_msgs.msg import Twist, Vector3
 
 from turtle_interfaces.srv import Waypoints
 
-from turtlesim.srv import TeleportAbsolute
+from turtlesim.msg import Pose
+
+from turtlesim.srv import TeleportAbsolute, SetPen
 
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
@@ -26,8 +30,12 @@ def turtle_twist(linear_vel, angular_vel):
     return Twist(linear = Vector3(x = linear_vel[0], y = linear_vel[1], z = linear_vel[2]),
                     angular = Vector3(x = angular_vel[0], y = angular_vel[1], z = angular_vel[2]))
 
-
-
+def compute_distance(waypoints):
+    total_dist = 0.0
+    for i in range(0,len(waypoints)-1):
+        total_dist += math.sqrt((waypoints[i].x - waypoints[i+1].x)**2 + (waypoints[i].y - waypoints[i+1].y)**2)
+    
+    return total_dist
 
 class Waypoint(Node):
     """The waypoint Node"""
@@ -36,8 +44,9 @@ class Waypoint(Node):
         super().__init__('waypoint')
         self.get_logger().info('waypoint_node')
 
-        self.pub = self.create_publisher(Twist, "cmd_vel", 10)
+        self.pub = self.create_publisher(Twist, "cmd_vel", 1)
 
+        self.sub = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10)
 
         self.declare_parameter('frequency',90)
         # Can change the parameter value during runtime but change is not effective
@@ -54,19 +63,23 @@ class Waypoint(Node):
 
         self._reset_client = self.create_client(Empty, "reset", callback_group=self.cbgroup)
         
-
         self._teleport_client = self.create_client(TeleportAbsolute, "/turtle1/teleport_absolute", callback_group=self.cbgroup)
         
+        self._pen_client = self.create_client(SetPen, "/turtle1/set_pen", callback_group=self.cbgroup)
 
     def timer_callback(self):
         tim_turt_state = self.get_parameter('turtle_state').get_parameter_value().string_value
         if(tim_turt_state == 'MOVING'):
 
             self.get_logger().debug("Issuing Command!") 
-            twist = turtle_twist([4.5, 0.0, 0.0], [0.0, 0.0, -2.0])
-            self.pub.publish(twist)
-            # self._pose_pub.publish()
+            # twist = turtle_twist([4.5, 0.0, 0.0], [0.0, 0.0, -2.0])
+            # self.pub.publish(twist)
     
+    def pose_callback(self, msg):
+        # self.get_logger().info("Theta value is: '%s'" %msg.theta)
+        pass
+
+
     def toggle_callback(self,request,response):
         turt_state = self.get_parameter('turtle_state').get_parameter_value().string_value
         if(turt_state == 'MOVING'):
@@ -95,11 +108,52 @@ class Waypoint(Node):
         
         tel_pos = TeleportAbsolute.Request()
 
+        while not self._pen_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Set Pen service not available, waiting again")
+
+
+
+        pen_status = SetPen.Request()
+        pen_status.r = 255
+        pen_status.g = 255
+        pen_status.b = 255
+        pen_status.width = 4
+
+ 
         for i in range(0,len(request.waypoints)):
+            pen_status.off = 255
+            await self._pen_client.call_async(pen_status)
             tel_pos.x = request.waypoints[i].x
             tel_pos.y = request.waypoints[i].y      
             await self._teleport_client.call_async(tel_pos)
-            # write code to make turtlesim sleep
+            # write code to make turtlesim draw X
+            pen_status.off = 0
+            await self._pen_client.call_async(pen_status)
+            tel_pos.x = request.waypoints[i].x + 0.2
+            tel_pos.y = request.waypoints[i].y - 0.2
+            await self._teleport_client.call_async(tel_pos)
+            tel_pos.x = request.waypoints[i].x - 0.2 
+            tel_pos.y = request.waypoints[i].y + 0.2
+            await self._teleport_client.call_async(tel_pos)
+            pen_status.off = 255
+            await self._pen_client.call_async(pen_status)
+            tel_pos.x = request.waypoints[i].x - 0.2
+            tel_pos.y = request.waypoints[i].y - 0.2
+            await self._teleport_client.call_async(tel_pos)
+            pen_status.off = 0
+            await self._pen_client.call_async(pen_status)
+            tel_pos.x = request.waypoints[i].x + 0.2
+            tel_pos.y = request.waypoints[i].y + 0.2
+            await self._teleport_client.call_async(tel_pos)
+
+        pen_status.off = 255
+        await self._pen_client.call_async(pen_status)
+        tel_pos.x = request.waypoints[0].x
+        tel_pos.y = request.waypoints[0].y
+        await self._teleport_client.call_async(tel_pos)
+
+        self.toggle_callback(Empty.Request(),Empty.Response())
+        response.distance = compute_distance(request.waypoints)
         self.get_logger().info("Waypoint Callback Done!")
         return response
 
